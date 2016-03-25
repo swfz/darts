94e0ba8ce25c1f7b8ff43b6b6153d9d7197ef8b9
swfz <sawafuji.09@gmail.com>
2016-03-25 22:34:19 +0900

    add column script

---
+++ modify_json.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'tapp'
+
+def read(file)
+  return [] if !File.exist?(file)
+
+  json = open(file) do |io|
+    JSON.load(io)
+  end
+
+  return json
+end
+
+file = 'data/darts.json'
+data = read(file)
+data.each{|r|
+  r["stats"] = r["score"] / 8 .to_f if r["game"] == 'countup'
+}
+
+puts JSON.pretty_generate(data, options = nil)
+
+
